c66fbfa5b2054653bebd53ff8c5110fbe072a939
patrick.123.foster@gmail.com <patrick.123.foster@gmail.com>
2023-06-10 13:04:50 -0700

    cors debug

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,9 +1,9 @@
 const TESTING = false;
 const constants = {
   url: TESTING ? "http://localhost:8000" : "https://langface.up.railway.app",
-  localUrl: TESTING ? "http://localhost:3000" : "https://langface.ai",
+  localUrl: "http://localhost:3000",
+  // localUrl: TESTING ? "http://localhost:3000" : "https://langface.ai",
   WP_CLIENT_ID: 87563,
-  WP_REDIRECT_URI: TESTING ? "http://localhost:3000" : "https://langface.ai",
 };
 
 export default constants;
