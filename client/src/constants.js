3e6ec4a135ceb54091f58d90d3e887afa60441e9
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2023-06-10 15:19:28 -0400

    clean up meeting

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
-const TESTING = true;
+const TESTING = false;
 const constants = {
   url: TESTING ? "http://localhost:8000" : "https://langface.up.railway.app",
-  localUrl: TESTING ? "http://localhost:3000" : "https://langface.netlify.app",
+  localUrl: TESTING ? "http://localhost:3000" : "https://langface.ai",
   WP_CLIENT_ID: 87563,
   WP_REDIRECT_URI: "http://localhost:3000",
 };
